09cb5a49b8e7438e50918261988e33658d500612
ElliotRedhead <elliot.redhead@gmail.com>
2021-08-16 21:40:24 +0100

    Refactor project with new name

--- django_graphene/cookbook/asgi.py
+++ django_graphene/cookbook/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_graphene project.
+ASGI config for cookbook project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_graphene.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cookbook.settings")
 
 application = get_asgi_application()
--- django_graphene/django_graphene/schema.py
+++
@@ -1,8 +0,0 @@
-import graphene
-
-
-class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hello world")
-
-
-schema = graphene.Schema(query=Query)
