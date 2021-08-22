d64db5bf512f524007fb5ee496f9f86722642363
ElliotRedhead <elliot.redhead@gmail.com>
2021-08-22 01:17:03 +0100

    Make category field optional on ingredient model

--- django_graphene/ingredients/models.py
+++ django_graphene/ingredients/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
     notes = models.TextField()
     category = models.ForeignKey(
-        Category, related_name="ingredients", on_delete=models.CASCADE
+        Category, related_name="ingredients", on_delete=models.CASCADE, null=True
     )
 
     def __str__(self):
